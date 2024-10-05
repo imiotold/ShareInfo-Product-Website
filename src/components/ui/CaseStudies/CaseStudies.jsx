79a832f676528315d0c404f4b9f1c53dcd8afdf9
muhdfarseen <muhdfarseen@gmail.com>
2024-10-06 00:07:39 +0530

    99%

--- src/components/ui/CaseStudies/CaseStudies.jsx
+++ src/components/ui/CaseStudies/CaseStudies.jsx
@@ -90,12 +90,12 @@ const caseStudyData = [
         date: 'May 23',
         description: 'Real-World Learning for Industry Success',
         imageUrl:
-            'https://images.unsplash.com/photo-1687463221023-02f259da7d77?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         date: 'June 10',
         description: 'Industry-Academia Collaboration',
         imageUrl:
-            'https://images.unsplash.com/photo-1687463221020-b8769b32c622?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            'https://images.unsplash.com/photo-1488998427799-e3362cec87c3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
 ];
